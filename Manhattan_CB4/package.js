8773f425f6bfd479a53f516f0fa8c2f6447471af
chilinhhovo <cv2604@columbia.edu>
2024-12-13 15:03:03 -0500

    js added

---
+++ Manhattan_CB4/package.js
@@ -0,0 +1,25 @@
+"scripts": {
+  "start": "node index.js",
+  "test": "echo \"Error: no test specified\" && exit 1"
+}
+{
+    "name": "c",
+    "version": "1.0.0",
+    "description": "",
+    "main": "index.js",
+    "scripts": {
+      "start": "node index.js",
+      "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "repository": {
+      "type": "git",
+      "url": "git+https://github.com/chilinhhovo/auto_update_web.git"
+    },
+    "author": "",
+    "license": "ISC",
+    "bugs": {
+      "url": "https://github.com/chilinhhovo/auto_update_web/issues"
+    },
+    "homepage": "https://github.com/chilinhhovo/auto_update_web#readme"
+  }
+  
